e43172fe41aa3ca4b50f85a58874a0822ef238ed
chrajeshn <chrajeshn@gmail.com>
2024-05-25 22:18:32 +0530

    terraform-ec2-test

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "crn503-remote-state"
+    key    = "expense-vpc"
+    region = "us-east-1"
+    dynamodb_table = "crn503-locking"
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
